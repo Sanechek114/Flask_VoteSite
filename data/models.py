ddf69f06c5c6cc66469e717ab758ed449038da17
Aleksander <ermolaevsasa114@gmail.com>
2026-05-13 20:07:59 +0300

    Revert "Код + Пояснительная записка + Презентация + Почти все работает"
    This reverts commit cc631ce2e7849f9e6a55bc03bb707f22c7aa3f76.

--- data/models.py
+++ data/models.py
@@ -1,4 +1,3 @@
-# data/models.py
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
@@ -7,7 +6,6 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)  # 👈 ПОЛЕ ЗДЕСЬ
     polls = db.relationship('Poll', backref='author', lazy=True)
 
     def set_password(self, pwd): self.password_hash = generate_password_hash(pwd)
@@ -23,10 +21,11 @@ class Option(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     poll_id = db.Column(db.Integer, db.ForeignKey('poll.id'), nullable=False)
     text = db.Column(db.String(100), nullable=False)
-    votes = db.relationship('Vote', backref='option', lazy='dynamic', cascade='all, delete-orphan')
+    votes = db.relationship('Vote', backref='option', lazy='dynamic')
+
 
 class Vote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    option_id = db.Column(db.Integer, db.ForeignKey('option.id', ondelete='CASCADE'), nullable=False)
+    option_id = db.Column(db.Integer, db.ForeignKey('option.id'), nullable=False)
     __table_args__ = (db.UniqueConstraint('user_id', 'option_id'),)
