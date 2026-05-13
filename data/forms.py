2f6007d7d651d3d39c1fb6c37d18ec60333a78b0
Aleksander <ermolaevsasa114@gmail.com>
2026-05-13 19:45:32 +0300

    Код + Пояснительная записка + Презентация + Почти все работает 2

--- data/forms.py
+++ data/forms.py
@@ -1,24 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
-
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Length, EqualTo, NumberRange
 
 class RegForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired(), Length(3, 20)])
-    password = PasswordField('Пароль', validators=[DataRequired(), Length(4, 20)])
+    username = StringField('Логин', validators=[DataRequired(), Length(3,20)])
+    password = PasswordField('Пароль', validators=[DataRequired(), Length(4,20)])
     confirm = PasswordField('Повтор', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Зарегистрироваться')
-
 
 class LoginForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Войти')
 
-
-class PollForm(FlaskForm):
-    title = StringField('Название опроса', validators=[DataRequired()])
-    opt1 = StringField('Вариант 1', validators=[DataRequired()])
-    opt2 = StringField('Вариант 2', validators=[DataRequired()])
-    opt3 = StringField('Вариант 3')
-    submit = SubmitField('Создать')
+class PollConfigForm(FlaskForm):
+    title = StringField('Название опроса', validators=[DataRequired(), Length(3,100)])
+    num_options = IntegerField('Количество вариантов', validators=[DataRequired(), NumberRange(min=2, max=10)])
+    submit = SubmitField('Далее →')
